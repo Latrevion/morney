84a49038370666508dd4ecf96e4149d8f485e34a
Latrevion <xiaopikaqiu@outlook.com>
2022-04-25 21:22:26 +0800

    三个页面重复一样的布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,10 @@
 <template>
-<div>labels.vue
-  <Nav />
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      labels.vue
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -13,5 +16,16 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,11 +1,15 @@
 <template>
-<div>statistics.vue
-  <Nav />
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      statistics.vue
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang='ts'>
 import Nav from '@/components/Nav.vue';
+
 export default {
   name: 'Statistics',
   // components: {Nav}
@@ -13,5 +17,16 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
