0c580f3d8528dbc078dd2464f8db032ebb1e9f2f
Latrevion <xiaopikaqiu@outlook.com>
2022-04-25 21:54:46 +0800

    创建了layout组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      statistics.vue
-    </div>
-    <Nav/>
-  </div>
+  <layout>
+    statistics.vue
+  </layout>
 </template>
 
 <script lang='ts'>
@@ -16,17 +13,3 @@ export default {
 };
 </script>
 
-<style scoped lang='scss'>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.content {
-  border: 1px solid blue;
-  overflow: auto;
-  flex-grow: 1;
-}
-</style>
