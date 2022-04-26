ab64cb2fa7d0f67dd8fa95931d20635acf3885c9
Latrevion <xiaopikaqiu@outlook.com>
2022-04-26 22:45:00 +0800

    创建了<Icon/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账</router-link>
+    |
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签</router-link>
+    |
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计</router-link>
   </div>
 </template>
 
 <script lang='ts'>
+// import x from '@/assets/icons/label.svg';
+// import y from '@/assets/icons/money.svg';
+// console.log(x)
+
+
 export default {
-  name: 'NAV'
+  name: 'NAV',
 };
 </script>
 
 <style scoped lang='scss'>
-.nav{
+.nav {
+  display: flex;
   border: 1px solid red;
 }
 </style>
