511415755c4cf32a3f9788e95fb4eb4ca3637675
Latrevion <xiaopikaqiu@outlook.com>
2022-05-29 21:34:52 +0800

    修复进页面弹出四个提示的bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,7 @@
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
     <div class="notes">
-      <FormItem filed-name="备注"  placeholder="在这里输入备注" :value.sync="record.notes"/>
+      <FormItem filed-name="备注" placeholder="在这里输入备注" :value.sync="record.notes"/>
     </div>
     <Tags @update:valu.capture="record.tags=$event"/>
   </layout>
@@ -46,8 +46,7 @@ export default class Money extends Vue {
 
   saveRecord() {
     if (!this.record.tags || this.record.tags.length === 0) {
-      window.alert('请至少选择一个标签');
-      return;
+      return window.alert('请至少选择一个标签');
     }
     this.$store.commit('createRecord', this.record);
     if (this.$store.state.createRecordError === null) {
