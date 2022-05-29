9043df4ae302341702925e93b23701e58b7d7d61
Latrevion <xiaopikaqiu@outlook.com>
2022-05-29 21:50:21 +0800

    修复无法正常记账的错误

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,9 +3,12 @@
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
     <div class="notes">
-      <FormItem filed-name="备注" placeholder="在这里输入备注" :value.sync="record.notes"/>
+      <FormItem filed-name="备注"
+                placeholder="在这里输入备注"
+                :value.sync="record.notes"
+      />
     </div>
-    <Tags @update:valu.capture="record.tags=$event"/>
+    <Tags @update:value.capture="record.tags=$event"/>
   </layout>
 </template>
 
