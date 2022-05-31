3c2fd80f091f0ab8db99b34bb094547a159423e9
Latrevion <xiaopikaqiu@outlook.com>
2022-05-31 18:03:20 +0800

    继续优化二维码，增加遮罩效果，pc限制宽度正常使用

--- src/main.ts
+++ src/main.ts
@@ -28,12 +28,27 @@ window.onload = function () {
 if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打卡本页面，以保证浏览效果');
   const img = document.createElement('img');
+  const cover = document.createElement('div');
   img.src = './qrcode.png';
   img.style.position = 'fixed';
   img.style.left = '50%';
   img.style.top = '50%';
   img.style.transform = 'translate(-50%,-50%)';
   img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  cover.style.position = 'fixed';
+  cover.style.top = '0';
+  cover.style.left = '0';
+  cover.style.right = '0';
+  cover.style.bottom = '0';
+  cover.style.height = '100vh';
+  cover.style.width = '500px';
+  cover.style.marginLeft = '50%';
+  cover.style.transform = 'translate(-50%)';
+  cover.style.background = 'rgba(0, 0, 0, 0.5)';
+  document.body.appendChild(cover);
   document.body.appendChild(img);
+  cover.onclick = function () {
+    img.style.display = 'none';
+    cover.style.display = 'none';
+  };
 }
-
